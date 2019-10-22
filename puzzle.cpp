0839a75abb84ffe66fd779647d23124533a2d5ea
WeichiChen1210 <iphone31302@gmail.com>
2019-10-23 01:48:06 +0800

    Finish IDA* and printing moving process

--- puzzle.cpp
+++ puzzle.cpp
@@ -1,10 +1,11 @@
 #include <bits/stdc++.h>
+#include <algorithm>
 using namespace std;
 
-int puzzle[4][4], temp[16], zero_r, zero_c;
+int puzzle[4][4], origin[4][4], temp[16], zero_r, zero_c, temp_r, temp_c;
 bool even_parity = false;
 int goal_pos1[4][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 0}};
-int goal_pos2[16] = {1, 2, 3, 4, 8, 7, 6, 5, 9, 10, 11, 12, 0, 15, 14, 13};
+int goal_pos2[4][4] = {{1, 2, 3, 4}, {8, 7, 6, 5}, {9, 10, 11, 12}, {0, 15, 14, 13}};
 vector<char> path;
 int r_dir[4] = {0, 0, +1, -1};
 int c_dir[4] = {+1, -1, 0, 0};
@@ -34,11 +35,13 @@ void generate_puzzle(){
     for(int j = 0; j < 4; ++j){
         for(int i = 0; i < 4; ++i){
             puzzle[j][i] = temp[cnt++];
+            origin[j][i] = puzzle[j][i];
         }
     }
 
     print_puzzle();
 } 
+
 
 int check_parity(){
     int f1 = 0, f2 = 0, f = 0;
@@ -46,8 +49,8 @@ int check_parity(){
     for(int i = 0; i < 16; ++i){
         int num = temp[i];
         if(num == 0){
-            zero_r = i/4;
-            zero_c = i%4;
+            temp_r = zero_r = i/4;
+            temp_c = zero_c = i%4;
             f2 = zero_r + 1;
             continue;
         }
@@ -61,12 +64,12 @@ int check_parity(){
     }
     f = f1 + f2;
     // cout << "f: " << f << " " << f1 << " " << f2 << endl;
-    cout << "f: " << f << endl;
     if(f % 2 == 0)  even_parity = true;
     else even_parity = false;
-    cout << "parity: " << even_parity << endl;
+    // cout << "parity: " << even_parity << endl;
 }
 
+// H function, calculate Manhattan distance
 int H(){
     int distance = 0;
 
@@ -85,25 +88,34 @@ int H(){
 }
 
 int DFS(int g, int prev_direction, int threshold){
-    cout << "g prev thr " << g << " " << prev_direction << " " << threshold << endl; 
+    // cout << "g prev thr " << g << " " << prev_direction << " " << threshold << endl;
+    // update h
     int h = H();
     if(h == 0)  return -1;   // found
-    if((g+h) > threshold) return g+h;     // fail
+    if((g+h) > threshold) return g+h; // return and increase threshold
 
     int mn = 100000000;
+    //  test four directions
     for(int i = 0; i < 4; ++i){
+        // avoid going back
         if(i == (prev_direction ^ 1))   continue;
 
+        // calculate the next position to move
         int next_r = zero_r + r_dir[i];
         int next_c = zero_c + c_dir[i];
         if(next_r < 0 || next_r > 4 || next_c < 0 || next_c > 4) continue;
 
+        // swap position
         path.push_back(dir[i]);
         swap(puzzle[next_r][next_c], puzzle[zero_r][zero_c]);
         swap(next_r, zero_r);
         swap(next_c, zero_c);
+
+        // DFS to go down
         int result = DFS(g+1, i, threshold);
+        // if return -1 then success
         if(result == -1) return -1;
+        // else this move is not good, swap back
         if(result < mn) mn = result;
         swap(next_r, zero_r);
         swap(next_c, zero_c);
@@ -113,36 +125,98 @@ int DFS(int g, int prev_direction, int threshold){
     return mn;
 }
 
+// IDA*
 bool IDAstar(){
+    // set up threshold with H()
     int threshold = H();
-    cout << threshold << endl;
+    // cout << threshold << endl;
     while(1){
+        // DFS search
         int t = DFS(0, -1, threshold);
-        cout << "t " << t << endl;
-        if(t == -1)   return true;
+        // cout << "t " << t << endl;
+        if(t == -1)   return true;  // success
         if(t == 100000000) return false;
-        if(t > 70) return false;
+        if(t > 60) return false;
+        // increase depth and search again
         threshold = t;
     }
     return false;
 }
 
-int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
+// print out the moving processes
+void print_move(){
+    int steps = path.size();
+    // cout << "steps: " << steps << endl;
+    // resume the zero position
+    zero_r = temp_r;
+    zero_c = temp_c;
 
+    for(int i = 0; i < steps; ++i){        
+        char temp = path[i];
+        int direction;
+        cout << "============\nStep " << i+1 << ": " << temp << endl;
+        // select direction
+        switch (temp){
+            case 'R':
+                direction = 0;
+                break;
+            case 'L':
+                direction = 1;
+                break;
+            case 'D':
+                direction = 2;
+                break;
+            case 'U':
+                direction = 3;
+                break;
+        }
+        // swap blocks
+        int next_r = zero_r + r_dir[direction];
+        int next_c = zero_c + c_dir[direction];
+        swap(origin[next_r][next_c], origin[zero_r][zero_c]);
+        swap(next_r, zero_r);
+        swap(next_c, zero_c);
+
+        // print out puzzle
+        for(int j = 0; j < 4; ++j){
+            for(int i = 0; i < 4; ++i)
+                cout << setw(3) << origin[j][i];
+            cout << endl;
+        }
+
+    }
+
+}
+
+int main(){
     int m_distance = 0;
     cout << "Random puzzle:\n";
     generate_puzzle();
     check_parity();
 
     m_distance = H();
-    cout << "Manhatann distance: " << m_distance << endl;
-    if(!even_parity) return 0;
+    // cout << "Manhatann distance: " << m_distance << endl;
+    // odd
+    if(!even_parity){
+        cout << "this puzzle is not solvable\n";
+        return 0;
+    }
+    // even
     else {
         cout << "even" << endl;
-        // cout << IDAstar() << endl;
-        // print_puzzle();
+        bool result = IDAstar();
+        // too many steps
+        if(!result){
+            cout << "this puzzle is not solvable\n";
+            return 0;
+        }
+        // cout << result << endl;
+        for(int i = 0; i < path.size(); ++i)
+            cout << path[i] << " ";
+        cout << endl;
+        // print the moving process
+        print_move();
     }
+    
 
 }
